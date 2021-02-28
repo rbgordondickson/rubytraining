2485d4d1ed3d1a5519cfa615de215b6bc33bf0e2
Gordon Dickson <rb.gordondickson@gmail.com>
2021-02-28 12:56:54 +0000

    Lesson 4 done

--- lesson4.rb
+++ lesson4.rb
@@ -1,1 +1,10 @@
 # Lesson 4
+print "Pleathe enter a thtring: "
+user_input = gets.chomp
+user_input.downcase!
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  print "Nothing to change"
+end
+puts "Adios. #{user_input}"
